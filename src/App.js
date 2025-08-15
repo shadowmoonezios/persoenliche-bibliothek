913e58b901ff3a2842832ceaa4e5b7de7191f3fa
shadowmoonezios <agushifelde@hotmail.com>
2025-08-15 07:18:54 +0300

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Willkommen in deiner persönlichen Bibliothek</h1>
+      {/* Hier kann die Hauptkomponente der Anwendung hinzugefügt werden */}
+    </div>
+  );
+}
+
+export default App;
